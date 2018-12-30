3e684e8fa9620f124a0f44d026721b4722280cca
Thien <thienxuanle@yahoo.com>
2018-12-29 22:56:38 -0500

    project3

---
+++ Project3/intercomm/TraineeClient.java
@@ -0,0 +1,32 @@
+package com.revature.caliber.intercomm;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.revature.caliber.beans.Trainee;
+
+@FeignClient(name= "user-service", url = "localhost:9085/")
+public interface TraineeClient {
+	
+	
+	@GetMapping(value="/all/trainee", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Trainee>> findAllByBatch(@RequestParam("batch") Integer batch);
+	
+	@PutMapping(value="all/trainee/update", produces=MediaType.APPLICATION_JSON_VALUE)
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public ResponseEntity<Trainee> updateTrainee(@Valid @RequestBody Trainee trainee);
+	
+	
+}
